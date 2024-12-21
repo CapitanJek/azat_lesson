b3a48a6592dd7bec39a8964738bb782e8f3b2334
azsheishenov <a.sheyshenov@bk>
2024-12-21 20:17:24 +0600

    initialize new project + added prettier and eslint

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    semi: true,
+    trailingComma: 'all',
+    printWidth: 100,
+    tabWidth: 4,
+    singleQuote: true,
+    useTabs: false,
+    jsxSingleQuote: true,
+    endOfLine: 'auto',
+};
